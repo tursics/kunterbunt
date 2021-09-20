428047ee0a770ebf4ac24bd9985b13182f6aa4d7
Thomas Tursics <thomas@tursics.de>
2021-09-20 23:49:20 +0200

    init some html stuff

---
+++ index.js
@@ -0,0 +1,44 @@
+var fillColor = '#fff';
+
+function initEventsToReset(svg, className) {
+    var elems = document.getElementsByClassName(className);
+
+    if (elems.length > 0) {
+        elems[0].addEventListener('click', function() {
+            var pathes = svg.querySelectorAll('path');
+
+            for (p = 0; p < pathes.length; ++p) {
+                pathes[p].setAttribute('fill', '#fff');
+            }
+        });
+    }
+}
+
+function initEventsToSwatches(className) {
+    var elems = document.getElementsByClassName(className);
+
+    for (var e = 0; e < elems.length; ++e) {
+        elems[e].addEventListener('click', function() {
+            var actives = document.getElementsByClassName('active');
+            if (actives.length > 0) {
+                actives[0].classList.remove('active');
+            }
+            this.classList.add('active');
+            fillColor = window.getComputedStyle(this).getPropertyValue('background-color');
+        });
+    }
+}
+
+function initEventsOfVector(svg) {
+    var pathes = svg.querySelectorAll('path');
+
+    for (p = 0; p < pathes.length; ++p) {
+        pathes[p].addEventListener('click', function() {
+            this.setAttribute('fill', fillColor);
+        });
+    }
+}
+
+initEventsToReset(document.querySelector('svg'), 'reset');
+initEventsToSwatches('swatch');
+initEventsOfVector(document.querySelector('svg'));
