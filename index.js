50952dc81e3211bdcf645aef6bd24bc5694d84c7
Thomas Tursics <thomas@tursics.de>
2021-09-23 17:45:13 +0200

    color duck

--- index.js
+++ index.js
@@ -1,50 +1,60 @@
-var fillColor = '#fff';
+var tool = {
+	buttons: {
+		resetCanvasColors: null,
+	},
+	canvas: {
+		dom: null,
+		doc: null,
+		id: '',
+	},
+	color: '#fff',
+};
 
-function initEventsToReset(svg, className) {
-    var elems = document.getElementsByClassName(className);
+function onButtonResetCanvasColors() {
+	var pathes = tool.canvas.doc.querySelectorAll('path');
 
-    if (elems.length > 0) {
-        elems[0].addEventListener('click', function() {
-            var svgDoc = svg.contentDocument;
-            var pathes = svgDoc.querySelectorAll('path');
-
-            console.log(svgDoc);
-
-            for (p = 0; p < pathes.length; ++p) {
-                pathes[p].setAttribute('style', 'fill:#fff');
-                // fill:#cfc6bb;fill-opacity:1;stroke:none;stroke-width:0.264583
-            }
-        });
-    }
+	for (p = 0; p < pathes.length; ++p) {
+		var props = 'fill:#fff;stroke:#aaa;stroke-width:0.1;';
+		pathes[p].setAttribute('style', props);
+	}
 }
 
-function initEventsToSwatches(className) {
-    var elems = document.getElementsByClassName(className);
-
-    for (var e = 0; e < elems.length; ++e) {
-        elems[e].addEventListener('click', function() {
-            var actives = document.getElementsByClassName('active');
-            if (actives.length > 0) {
-                actives[0].classList.remove('active');
-            }
-            this.classList.add('active');
-            fillColor = window.getComputedStyle(this).getPropertyValue('background-color');
-        });
-    }
+function onCanvasPath() {
+	var props = 'fill:' + tool.color + ';stroke:none;';
+	this.setAttribute('style', props);
 }
 
-function initEventsOfVector(svg) {
-    var pathes = svg.querySelectorAll('path');
+function onButtonColorSwatch() {
+	var actives = document.getElementsByClassName('active');
+	if (actives.length > 0) {
+		actives[0].classList.remove('active');
+	}
+	this.classList.add('active');
+	tool.color = window.getComputedStyle(this).getPropertyValue('background-color');
+}
 
+function initCanvas(idCanvas) {
+	tool.canvas.id = idCanvas;
+	tool.canvas.dom = document.getElementById(tool.canvas.id);
+	tool.canvas.doc = tool.canvas.dom.contentDocument;
+
+    var pathes = tool.canvas.doc.querySelectorAll('path');
     for (p = 0; p < pathes.length; ++p) {
-        pathes[p].addEventListener('click', function() {
-            this.setAttribute('fill', fillColor);
-        });
+        pathes[p].addEventListener('click', onCanvasPath);
     }
+}
+
+function initButtons() {
+	tool.buttons.resetCanvasColors = document.getElementById('resetColor');
+	tool.buttons.resetCanvasColors.addEventListener('click', onButtonResetCanvasColors);
+
+    var swatches = document.getElementsByClassName('swatch');
+    for (var s = 0; s < swatches.length; ++s) {
+        swatches[s].addEventListener('click', onButtonColorSwatch);
+	}
 }
 
 window.addEventListener('load', function() {
-    initEventsToReset(document.getElementById('graphic'), 'reset');
-    initEventsToSwatches('swatch');
-    initEventsOfVector(document.querySelector('svg'));
+	initButtons();
+	initCanvas('graphic');
 });
