2cebf2f6033b9d0e8a58084acd65e0a8467867fd
Thomas Tursics <thomas@tursics.de>
2021-10-06 08:39:59 +0200

    remove the (not more needed) big buttons

--- js/index.js
+++ js/index.js
@@ -1,30 +1,11 @@
-function onButtonResetCanvasColors() {
-	var pathes = tool.canvas.doc.querySelectorAll('path');
-
-	for (p = 0; p < pathes.length; ++p) {
-		var props = 'fill:#fff;stroke:#aaa;stroke-width:0.1;';
-		pathes[p].setAttribute('style', props);
-	}
-
-	undo.reset();
-}
-
-function initButtons() {
-	buttons.init();
-
-	tool.buttons.resetCanvasColors = document.getElementById('resetColor');
-	tool.buttons.resetCanvasColors.addEventListener('click', onButtonResetCanvasColors);
-
-	buttons.update();
-}
-
 window.addEventListener('load', function() {
 	tool.colorPalette = mediaColorSwatch;
 	tool.files = mediaConfig;
 
 	paint.init();
 
-	initButtons();
+	buttons.init();
+	buttons.update();
 
 	dialog.init();
 
