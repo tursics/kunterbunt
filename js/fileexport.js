f373518e392b2ed027ce1f7da903b6e75936edea
Thomas Tursics <thomas@tursics.de>
2021-10-14 00:35:21 +0200

    bugfix social sharing on mobile devices

--- js/fileexport.js
+++ js/fileexport.js
@@ -3,7 +3,7 @@ var fileexport = {
 		callback(new Blob([tool.canvas.svg.outerHTML], {type: 'image/svg+xml;charset=utf-8'}));
 	},
 
-	toCanvas: function(callback) {
+	toCanvas: function(eraseBackground, callback) {
 		fileexport.toSVGBlob(function (blob) {
 			var URL = window.URL || window.webkitURL || window;
 			var blobURL = URL.createObjectURL(blob);
@@ -24,6 +24,10 @@ var fileexport = {
 				canvas.height = bbox.height * factor;
 
 				var context = canvas.getContext('2d');
+				if (eraseBackground) {
+					context.fillStyle = 'white';
+					context.fillRect(0, 0, canvas.width, canvas.height);
+				}
 				context.drawImage(image, 0, 0, canvas.width, canvas.height);
 
 				callback(canvas);
@@ -34,28 +38,36 @@ var fileexport = {
 	},
 
 	toPNGImage: function(callback) {
-		fileexport.toCanvas(function (canvas) {
+		fileexport.toCanvas(false, function (canvas) {
 			callback(canvas.toDataURL());
 		});
 	},
 
 	toJPGImage: function(callback) {
-		fileexport.toCanvas(function (canvas) {
+		fileexport.toCanvas(true, function (canvas) {
 			callback(canvas.toDataURL('image/jpg'));
 		});
 	},
 
 	toWebPImage: function(callback) {
-		fileexport.toCanvas(function (canvas) {
+		fileexport.toCanvas(true, function (canvas) {
 			callback(canvas.toDataURL('image/webp'));
 		});
 	},
 
 	toPNGBlob: function(callback) {
-		fileexport.toCanvas(function (canvas) {
+		fileexport.toCanvas(false, function (canvas) {
 			canvas.toBlob(function (blob) {
 				callback(blob);
 			}, 'image/png');
+		});
+	},
+
+	toJPGBlob: function(callback) {
+		fileexport.toCanvas(true, function (canvas) {
+			canvas.toBlob(function (blob) {
+				callback(blob);
+			}, 'image/jpeg', 0.95);
 		});
 	},
 
@@ -76,13 +88,23 @@ var fileexport = {
 	},
 
 	export: function() {
+		var filenamePNG = tool.canvas.title + '.png';
+		var filenameJPG = tool.canvas.title + '.jpg';
+
 		if (fileexport.canShare()) {
-			fileexport.toPNGBlob(function (blob) {
-				navigator.share({blob: blob, mimeType: 'image/png'});
+			fileexport.toJPGBlob(function (blob) {
+				const fileArray = [new File([blob], filenameJPG, {type: blob.type})];
+				const shareData = {
+					title: tool.canvas.title,
+					text: tool.canvas.title + ', ' + tool.canvas.attribtion.innerHTML,
+					files: fileArray,
+				};
+
+				navigator.share(shareData);
 			});
 		} else {
 			fileexport.toPNGImage(function (img) {
-				fileexport.download(img, tool.canvas.title + '.png');
+				fileexport.download(img, filenamePNG);
 			});
 		}
 	},
