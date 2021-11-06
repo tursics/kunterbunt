4c9bc5198910465612ba5367b34c8cddd6cdfefe
Thomas Tursics <thomas@tursics.de>
2021-11-06 10:50:58 +0100

    bugfix: 100vh is not the best idea on mobile

--- js/index.js
+++ js/index.js
@@ -4,11 +4,22 @@ function preventScrolling() {
 	}, false); 
 }
 
+function fixViewPortHeight() {
+	function fix() {
+		var vh = window.innerHeight;
+		document.documentElement.style.setProperty('--vh', vh + 'px');
+	}
+
+	window.addEventListener('resize', fix);
+	fix();
+}
+
 window.addEventListener('load', function() {
 	tool.colorPalette = mediaColorSwatch;
 	tool.files = mediaConfig;
 
 	preventScrolling();
+	fixViewPortHeight();
 
 	paint.init();
 	dialog.init();
